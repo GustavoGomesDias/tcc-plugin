1ed15611627c2ca6465788128574749082228130
hilariooliveira <hilariotomaz@gmail.com>
2023-08-16 15:48:10 -0300

    Alteração no script de extração das features.

--- run_generation_desc_json.py
+++ run_generation_desc_json.py
@@ -1,3 +1,4 @@
 from src.services.description.generate_description import generate_description
+
 if __name__ == '__main__':
     generate_description('python', test_mode=True)
--- src/services/tokens/Tokenize.py
+++ src/services/tokens/Tokenize.py
@@ -1,13 +1,25 @@
 import javalang
 import io
 import tokenize
+
 from token import tok_name
 from src.utils.types.Language import Language
+
 
 class Tokenize:
     def __init__(self, language: Language) -> None:
         self.language = language
-        self.dict_type = dict()
+        self.total_tokens = 0
+        if language == 'java':
+            self.dict_type = {
+                'tipo_1': 0
+                'tipo_2': 0
+            }
+        elif language == 'python':
+             self.dict_type = {
+                'tipo_1': 0
+                'tipo_2': 0
+            }
 
     def count_token_python(self, code):
         buffer = io.StringIO(code)
@@ -29,12 +41,15 @@ class Tokenize:
                 self.dict_type[token_type] = 1
             else:
                 self.dict_type[token_type] += 1
-
+        self.total_tokens = len(tokens)
+        
     def count_token_type(self, lst_code) -> dict:
         for code in lst_code:
-
             if self.language == Language.JAVA:
                 self.count_token_java(code)
             else:
                 self.count_token_python(code)
-        return self.dict_type
+        return {
+            'tokens_type': self.dict_type,
+            'total_tokens': self.total_tokens
+        }
