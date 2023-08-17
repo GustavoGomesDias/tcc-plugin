bb1cc0c96e1a7447400f88c80a597e0ba5be3002
GustavoCedisa <gustavo.dias@cedisa.com.br>
2023-08-17 15:12:00 -0300

    feat: add make dict type function

--- src/services/tokens/Tokenize.py
+++ src/services/tokens/Tokenize.py
@@ -1,6 +1,7 @@
 import javalang
 import io
 import tokenize
+from src.utils.helpers import make_dict_types
 
 from token import tok_name
 from src.utils.types.Language import Language
@@ -10,20 +11,12 @@ class Tokenize:
     def __init__(self, language: Language) -> None:
         self.language = language
         self.total_tokens = 0
-        if language == 'java':
-            self.dict_type = {
-                'tipo_1': 0
-                'tipo_2': 0
-            }
-        elif language == 'python':
-             self.dict_type = {
-                'tipo_1': 0
-                'tipo_2': 0
-            }
+        self.dict_type = make_dict_types(language)
 
     def count_token_python(self, code):
         buffer = io.StringIO(code)
         tokens = tokenize.generate_tokens(buffer.readline)
+        count = 0
         for token in tokens:
             token_type = tok_name[token.type]
 
@@ -31,17 +24,20 @@ class Tokenize:
                 self.dict_type[token_type] = 1
             else:
                 self.dict_type[token_type] += 1
+            count += 1
+        self.total_tokens = count
 
     def count_token_java(self, code):
         tokens = list(javalang.tokenizer.tokenize(code, ignore_errors=True))
-
+        count = 0
         for token in tokens:
             token_type = str(token).split(' ')[0]
             if token_type not in self.dict_type.keys():
                 self.dict_type[token_type] = 1
             else:
                 self.dict_type[token_type] += 1
-        self.total_tokens = len(tokens)
+            count += 1
+        self.total_tokens = count
         
     def count_token_type(self, lst_code) -> dict:
         for code in lst_code:
